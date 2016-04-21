dc1ff0a30f3a95cf46e93739ba75eae79d262bdf
AshokKumar54 <ashokkumar.mishra@sjsu.edu>
2016-04-20 21:56:51 -0700

    removed window variable for showing emulator

--- dqn/train_agent.lua
+++ dqn/train_agent.lua
@@ -1,3 +1,8 @@
+--[[
+Copyright (c) 2014 Google Inc.
+
+See LICENSE file for full terms of limited license.
+]]
 
 if not dqn then
     require "initenv"
@@ -73,7 +78,6 @@ local episode_reward
 local screen, reward, terminal = game_env:getState()
 
 print("Iteration ..", step)
-local win = nil
 while step < opt.steps do
     step = step + 1
     local action_index = agent:perceive(reward, screen, terminal)
@@ -88,9 +92,6 @@ while step < opt.steps do
             screen, reward, terminal = game_env:newGame()
         end
     end
-
-    -- display screen
-    win = image.display({image=screen, win=win})
 
     if step % opt.prog_freq == 0 then
         assert(step==agent.numSteps, 'trainer step: ' .. step ..
@@ -117,9 +118,6 @@ while step < opt.steps do
 
             -- Play game in test mode (episodes don't end when losing a life)
             screen, reward, terminal = game_env:step(game_actions[action_index])
-
-            -- display screen
-            win = image.display({image=screen, win=win})
 
             if estep%1000 == 0 then collectgarbage() end
 
